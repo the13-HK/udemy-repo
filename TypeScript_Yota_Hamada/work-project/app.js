be93121165f63fd70b8706965a182d11e7546ec4
HarukaKishida <thehflare@gmail.com>
2024-02-26 03:29:46 +0900

    セクション２完了

--- TypeScript_Yota_Hamada/work-project/app.js
+++ TypeScript_Yota_Hamada/work-project/app.js
@@ -1,1 +1,11 @@
-console.log("Hello, TypeScript!!!!");
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
---
+++ TypeScript_Yota_Hamada/work-project/basics.ts
@@ -0,0 +1,10 @@
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+} = {
+  name: "Yota",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
+console.log(person.name);
