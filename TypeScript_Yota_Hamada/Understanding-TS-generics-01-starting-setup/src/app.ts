81a1881ef088a9cc140d013505e6898c52ab96e9
HarukaKishida <thehflare@gmail.com>
2024-04-01 03:16:55 +0900

    セクション8まで完了

--- TypeScript_Yota_Hamada/Understanding-TS-generics-01-starting-setup/src/app.ts
+++ TypeScript_Yota_Hamada/Understanding-TS-generics-01-starting-setup/src/app.ts
@@ -1,1 +1,72 @@
 // Code goes here!
+
+function merge<T extends {}, U>(objA: T, objB: U, objC: U) {
+  return Object.assign(objA, objB, objC);
+}
+
+const mergedObj = merge(
+  { name: "Max" },
+  { age: 30 },
+  { hobbies: ["basket ball", "game"] }
+);
+console.log(mergedObj);
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements.`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Hi there!", "Hello!"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+extractAndConvert({ name: "Max" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Anna"];
